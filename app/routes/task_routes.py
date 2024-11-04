5cf4d3c9a0ec7bdc6e931a68892b6faf0a51d726
Liubov Dav <lubasha0512@gmail.com>
2024-11-03 20:52:09 -0800

    Added endpoints GET(tasks), GET (task), PUT, DELETE. Enabled tests.

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -29,6 +29,90 @@ def create_task():
     }
     return response, 201
 
+@task_bp.get("")
+def get_all_task():
+    query = db.select(Task)
+
+    title_param = request.args.get("title")
+    if title_param:
+        query = query.where(Task.title.ilike(f"%{title_param}%"))
+
+    description_param = request.args.get("description")
+    if description_param:
+        query = query.where(Task.description.ilike(f"%{description_param}%"))
+
+    completed_at_param = request.args.get("completed_at")
+    if completed_at_param:
+        query = query.where(Task.completed_at.ilike(f"%{completed_at_param}%"))
+
+    query = query.order_by(Task.id)
+    task = db.session.scalars(query)
+    # We could also write the line above as:
+    # books = db.session.execute(query).scalars()
+
+    tasks_response = []
+    for task in task:
+        tasks_response.append(
+            {
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": task.completed_at != None
+            }
+        )
+    return tasks_response
+
+@task_bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_task(task_id)
+
+    return {
+        "task": {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.completed_at != None
+        }
+    }
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        response = {"message": f"task {task_id} invalid"}
+        abort(make_response(response , 400))
+
+    query = db.select(Task).where(Task.id == task_id)
+    task = db.session.scalar(query)
+    
+    if not task:
+        response = {"message": f"task {task_id} not found"}
+        abort(make_response(response, 404))
+
+    return task
+
+@task_bp.put("/<task_id>")
+def update_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    task.completed_at = request_body["completed_at"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@task_bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_task(task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+
+
 
 
 
